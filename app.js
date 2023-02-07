40fcecd2de02d52a11d52c765e9b2a96b110fbd7
Nicolas Navarrete Maldonado <nikkolasnv1997@gmail.com>
2023-02-06 22:05:50 -0300

    feat: initial project structure

---
+++ app.js
@@ -0,0 +1,12 @@
+import express from "express"
+const app = express()
+
+app.get("/", (req, res) => {
+  res.send("Hello World!")
+})
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`)
+})
